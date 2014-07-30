1be35fba415e0b05429028a468ca793905494db8
Milan Simonovic <mbsimonovic@gmail.com>
2014-07-30 22:13:27 +0200

    longer timeouts, use prod views, try to reduce the doc size

--- src/test/java/com/milansimonovic/couchbase/AddedNotIndexedTest.java
+++ src/test/java/com/milansimonovic/couchbase/AddedNotIndexedTest.java
@@ -1,6 +1,7 @@
 package com.milansimonovic.couchbase;
 
 import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
 import com.couchbase.client.protocol.views.*;
 import net.spy.memcached.PersistTo;
 import net.spy.memcached.internal.OperationFuture;
@@ -29,8 +30,8 @@ public class AddedNotIndexedTest {
     private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(AddedNotIndexedTest.class);
 
     final String NODE = "192.168.1.128";
-    final String BUCKET = "test";
-    static final String designDocName = "testDesignDoc";
+    final String BUCKET = "default";
+    static final String designDocName = "designDoc";
     static final String viewName = "by_id";
     static final String docId = "some_id";
     CouchbaseClient client;
@@ -51,10 +52,10 @@ public class AddedNotIndexedTest {
 
     @BeforeClass
     public static void setup() {
+        assertTrue(docAsJson.length() > 0);
         //needs to be done BEFORE CouchbaseClient is initialized!
         System.setProperty("net.spy.log.LoggerImpl", "net.spy.memcached.compat.log.Log4JLogger");
-        System.setProperty("viewmode", "development");
-        assertTrue(docAsJson.length() > 0);
+        System.setProperty("viewmode", "production");
     }
 
     @Before
@@ -79,13 +80,16 @@ public class AddedNotIndexedTest {
         assertEquals(docAsJson, saved);
         assertEquals("saved but not indexed!?", 1, count());
         //double check: http://192.168.1.128:8092/test/_design/dev_testDesignDoc/_view/by_id?stale=false&connection_timeout=60000&limit=10
+
+        //mapreduce_errors log says:
+        //[mapreduce_errors:error,2014-07-30T22:12:51.659,ns_1@127.0.0.1:<0.25058.0>:couch_set_view_updater:load_doc:479]Bucket `default`, main group `_design/designDoc`, skipping document with ID `some_id`: too large body (1049883 bytes)
     }
 
     public long count() {
         final View view = client.getView(designDocName, viewName);
         Query query = new Query();
         query.setIncludeDocs(false);
-        query.setLimit(1);
+//        query.setLimit(1);
         query.setStale(Stale.FALSE);
         final ViewResponse response = client.query(view, query);
         return response.getTotalRows();
@@ -94,12 +98,20 @@ public class AddedNotIndexedTest {
 
     private CouchbaseClient openClient() throws IOException {
         List<URI> nodesList = Arrays.asList(URI.create("http://" + NODE + ":8091/pools"));
-        return new CouchbaseClient(nodesList, BUCKET, "");
+        CouchbaseConnectionFactoryBuilder builder = new CouchbaseConnectionFactoryBuilder();
+        builder.setOpTimeout(40000);
+        builder.setObsTimeout(55000);
+        builder.setViewTimeout(125000);
+
+        return new CouchbaseClient(builder.buildCouchbaseConnection(nodesList, BUCKET, ""));
     }
 
     private void createView() {
-        if (client.getDesignDoc(designDocName) != null) {
-            return;
+        try {
+            if (client.getDesignDoc(designDocName) != null) {
+                return;
+            }
+        } catch (Exception e) {
         }
         try {
             client.deleteDesignDoc(designDocName);
